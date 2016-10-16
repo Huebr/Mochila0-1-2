caa6a7d250b9b586766987b3f5dd9e6c59ec9f4d
pedro.jorge <pedro.jorge@aluno.uece.br>
2016-10-16 10:31:36 -0300

    mundança ta funfando para um bloco.

--- Mochila0-1/kernel.cu
+++ Mochila0-1/kernel.cu
@@ -12,14 +12,17 @@
 #include<math.h>
 #include <string.h>
 
-#define WEI 11
-#define ITEN 5
+//#define WEI 11
+//#define ITEN 5
 
-void inserirPeso(int vet[]);
-void inserirValor(int vet[]);
+//void inserirPeso(int vet[]);
+//void inserirValor(int vet[]);
+
+
 void info(int tam, int pes[], int val[], int n);
+void encItens(int *mol, int n, int W, int pes[], int val[], int nit, int wit);
 
-cudaError_t mochilaWithCuda(int *mochila, const int *peso, const int *valor);
+cudaError_t mochilaWithCuda(int *mochila, const int *peso, const int *valor, int iten, int pes);
 
 __global__ void mochilaKernel(int *mochila, const int *peso, const int *valor,const int wei,const int iten)
 {
@@ -32,13 +35,7 @@ __global__ void mochilaKernel(int *mochila, const int *peso, const int *valor,co
 
 			}
 			else{
-				if (mochila[(i - 1)*(wei + 1) + w] > valor[i] + mochila[(i - 1)*(wei + 1) + w - peso[i]]){
-					mochila[i*(wei + 1) + w] = mochila[(i - 1)*(wei + 1) + w];
-
-				}
-				else{
-					mochila[i*(wei + 1) + w] = valor[i] + mochila[(i - 1)*(wei + 1) + w - peso[i]];
-				}
+				mochila[i*(wei+1)+w] = max(mochila[(i - 1)*(wei+1)+w], valor[i] + mochila[(i - 1)*(wei+1)+ w - peso[i]]);
 
 			}
 		}
@@ -48,30 +45,71 @@ __global__ void mochilaKernel(int *mochila, const int *peso, const int *valor,co
 int main()
 {
     
+	char url[80];
+
 	//declaração do peso limite da mochila, o numero de itens e variavel auxiliar
-	int i, j, w;
+	int W, n, aux, aux2, q, i, j, *peso, *valor, *mochila;
+
+	//C:/Users/Pedro/Downloads/test.in
+
+	//---------------------Leitura do arquivo-----------------------------------------------
+
+	printf("Digite o local do arquivo: ");
+	printf("\n");
+	scanf("%s", &url);
+
+	FILE *arq;
+
+	arq = fopen(url, "r");
+	if (arq == NULL)
+		printf("Erro, nao foi possivel abrir o arquivo\n");
+	else{
+		fscanf(arq, "%d\n", &n);
+		aux = n + 1;
+
+		//Alocação
+		peso = (int*)malloc(aux*sizeof(int));
+		valor = (int*)malloc(aux*sizeof(int));
+		if (peso == NULL || valor == NULL){
+			perror("Erro de alocacao de memoria vetor peso ou vetor valor");
+			exit(EXIT_FAILURE);
+		}
+
+		for (i = 1; i<aux; i++){
+			fscanf(arq, "%d %d %d\n", &q, &valor[i], &peso[i]);
+		}
+
+		fscanf(arq, "%d\n", &W);
+		aux2 = W + 1;
+
+		mochila = (int*)malloc(aux*aux2*sizeof(int));
+		
+		if (mochila == NULL){
+			perror("Nao foi possivel alocar a mochila");
+			exit(EXIT_FAILURE);
+		}
+		for (i = 0; i < (aux)*(aux2); i++) mochila[i] = 0;
+
+	}
+
+	//Fecha o Arquivo
+	fclose(arq);
+	//------------------------Final do Arquvio------------------------------------------
 
 
-
-	//declação do peso e valor de cada item, e da matriz mochila
-	int peso[ITEN], valor[ITEN];
-
-	int *mochila;
-	mochila = (int*)malloc((ITEN + 1)*(WEI + 1)*sizeof(int));
-	for (i = 0; i < (ITEN + 1)*(WEI + 1); i++) mochila[i] = 0;
-
+	
 
 	//inserções dos itens com seus respectivos valores
-	inserirPeso(peso);
-	inserirValor(valor);
+	//inserirPeso(peso);
+	//inserirValor(valor);
 
-	info(WEI, peso, valor, ITEN);
+	info(W, peso, valor, n);
 
 	//Pseudo Codigo Transcrito
 
 
     // Add vectors in parallel.
-    cudaError_t cudaStatus = mochilaWithCuda(mochila,peso,valor);
+    cudaError_t cudaStatus = mochilaWithCuda(mochila,peso,valor,n,W);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "addWithCuda failed!");
         return 1;
@@ -89,10 +127,15 @@ int main()
 
 	printf("\n");
 
+	encItens(mochila, n, W, peso, valor, n, W);
+
+	printf("\n");
+	printf("\n");
+
 	//Imprimindo a matriz
-	for (i = 0; i<ITEN + 1; i++) {
-		for (j = 0; j<WEI + 1; j++) {
-			printf("%d ", mochila[(i*(WEI + 1)) + j]);
+	for (i = 0; i<n + 1; i++) {
+		for (j = 0; j<W + 1; j++) {
+			printf("%d ", mochila[(i*(W + 1)) + j]);
 
 		}
 		printf("\n"); // para pular linha quando terminar a coluna
@@ -103,15 +146,17 @@ int main()
 
 
 	printf("\n");
-	printf("Valor maximo da mochila: %d\n", mochila[ITEN*(WEI + 1) + WEI]);
+	printf("Valor maximo da mochila: %d\n", mochila[n*(W + 1) + W]);
 
 	free(mochila);
+	free(valor);
+	free(peso);
 	system("pause");
     return 0;
 }
 
 // Helper function for using CUDA to add vectors in parallel.
-cudaError_t mochilaWithCuda(int *host_mochila, const int *host_peso, const int *host_valor)
+cudaError_t mochilaWithCuda(int *host_mochila, const int *host_peso, const int *host_valor, int iten, int pes)
 {
     int *dev_mochila = 0;
     int *dev_peso = 0;
@@ -126,38 +171,38 @@ cudaError_t mochilaWithCuda(int *host_mochila, const int *host_peso, const int *
     }
 
     // Allocate GPU buffers for three vectors (two input, one output)    .
-	cudaStatus = cudaMalloc((void**)&dev_mochila,(ITEN + 1)*(WEI + 1)*sizeof(int));
+	cudaStatus = cudaMalloc((void**)&dev_mochila,(iten + 1)*(pes + 1)*sizeof(int));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
 
-    cudaStatus = cudaMalloc((void**)&dev_peso, ITEN * sizeof(int));
+    cudaStatus = cudaMalloc((void**)&dev_peso, (iten+1) * sizeof(int));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
 
-    cudaStatus = cudaMalloc((void**)&dev_valor, ITEN * sizeof(int));
+    cudaStatus = cudaMalloc((void**)&dev_valor, (iten+1) * sizeof(int));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
 
     // Copy input vectors from host memory to GPU buffers.
-	cudaStatus = cudaMemcpy(dev_mochila, host_mochila, (ITEN + 1)*(WEI + 1)* sizeof(int), cudaMemcpyHostToDevice);
+	cudaStatus = cudaMemcpy(dev_mochila, host_mochila, (iten + 1)*(pes + 1)* sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+	
+    cudaStatus = cudaMemcpy(dev_valor, host_valor, (iten+1) * sizeof(int), cudaMemcpyHostToDevice);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
     }
 
-    cudaStatus = cudaMemcpy(dev_valor, host_valor, ITEN * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-	cudaStatus = cudaMemcpy(dev_peso, host_peso , ITEN * sizeof(int), cudaMemcpyHostToDevice);
+	cudaStatus = cudaMemcpy(dev_peso, host_peso , (iten+1) * sizeof(int), cudaMemcpyHostToDevice);
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMemcpy failed!");
 		goto Error;
@@ -165,7 +210,7 @@ cudaError_t mochilaWithCuda(int *host_mochila, const int *host_peso, const int *
 
 
     // Launch a kernel on the GPU with one thread for each element.
-    mochilaKernel<<<1, WEI>>>(dev_mochila, dev_peso, dev_valor, WEI, ITEN);
+    mochilaKernel<<<1, pes>>>(dev_mochila, dev_peso, dev_valor, pes, iten);
 
     // Check for any errors launching the kernel
     cudaStatus = cudaGetLastError();
@@ -183,7 +228,7 @@ cudaError_t mochilaWithCuda(int *host_mochila, const int *host_peso, const int *
     }
 
     // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(host_mochila, dev_mochila, (ITEN+1)*(WEI+1)* sizeof(int), cudaMemcpyDeviceToHost);
+    cudaStatus = cudaMemcpy(host_mochila, dev_mochila, (iten+1)*(pes+1)* sizeof(int), cudaMemcpyDeviceToHost);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
@@ -242,4 +287,40 @@ void info(int tam, int pes[], int val[], int n){
 
 
 	printf("\n=========================================================\n");
+}
+
+void encItens(int *mol, int n, int W, int pes[], int val[], int nit, int wit){
+
+	int i, pcal;
+
+
+	//mat[n]
+
+	if (mol[(n*(wit+1))+W] == mol[((n - 1)*(wit+1))+W]){
+		if (mol[(n*(wit+1)) + W] != 0){
+			encItens(mol, n - 1, W, pes, val, nit, wit);
+		}
+	}
+	else{
+
+		for (i = 1; i<nit + 1; i++){
+			pcal = W - pes[i];
+			if (val[i] + mol[((i - 1)*(wit+1)) + pcal] == mol[(n*(wit+1)) + W]){
+				printf("%d ", i);
+				encItens(mol, i - 1, pcal, pes, val, nit, wit);
+				break;
+			}
+			if (mol[(n*(wit+1)) + W] == val[i]){
+				printf("%d ", i);
+
+				break;
+			}
+
+
+		}
+
+	}
+
+
+
 }
